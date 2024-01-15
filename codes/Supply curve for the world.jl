fee01b57ea51454869b0d9252cb2458e5dbf718b
xiaomingk <47573182+xiaomingk@users.noreply.github.com>
2024-01-15 16:33:32 +0100

    Update codes

--- codes/Supply curve for the world.jl
+++ codes/Supply curve for the world.jl
@@ -1,6 +1,6 @@
 #Renewable supply curve for the entire world under different discount rates
-using Plots, CSV, DataFrames
-S=CSV.read("supply curve.csv",DataFrame);
+using Plots, CSV, DataFrames, Plots.PlotMeasures
+S=CSV.read("supplyworld.csv",DataFrame);
 SS=Matrix(S);
 x=SS[:,1];
 y=SS[:,2];
@@ -8,12 +8,10 @@ x1=SS[:,3];
 y1=SS[:,4];
 x2=SS[:,5];
 y2=SS[:,6];
-x3=SS[:,7];
-y3=SS[:,8];
 
-fig=plot(x, y,label = "Country-specific discount rate", leg=:topleft, lw =1,size = (900, 500))
-fig=plot!(x1, y1,label = "Half country risk premium", leg=:topleft, lw =1,size = (900, 500))
-fig=plot!(x2, y2,label = "Uniform discount rate", leg=:topleft, xlabel = "Renewable energy supply potential [1000TWh]", ylabel = "RLCOE",color="green",lw =1)
-fig=plot!(x3, y3,label = "Electricity demand",color="orange",lw =1)
-fig=plot!(xlims=(0,500), ylims=(0, 150))
+fig=plot(x, y,label = "Uniform discount rate", leg=:topleft, xlabel = "Renewable energy supply potential [PWh]", ylabel = "Levelized cost of energy [\$\$/MWh]", lw =2, xlims=(0, 400), ylims=(0,50), guidefontsize=12,tickfontsize=12, legendfontsize=10, size = (900, 500), left_margin = 10mm, right_margin = 10mm, bottom_margin = 10mm)
+#fig=plot!(x1, y1,label = "Half country risk premium", leg=:topleft, lw =1,size = (900, 500))
+fig=plot!(x1, y1,label = "Country-specific discount rate", color="green",lw =2)
+fig=plot!(x2, y2,label = "Energy demand",color="orange",lw =2)
+#fig=plot!(xlims=(0,500), ylims=(0, 150))
 savefig(fig,"Renewable supply for the world.png")
